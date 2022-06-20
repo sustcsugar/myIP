26cb4f52d5370c6d3f33ba1952f77038179636ef
sugar <sg1144392179@gmail.com>
2022-06-21 00:29:16 +0800

    modify dqt_gen

--- cv_func/dqt_gen.py
+++ cv_func/dqt_gen.py
@@ -1,7 +1,4 @@
 import numpy as np
-from pandas import array
-
-
 
 def dqt_scaling(factor):
     std_dqt = np.array([
@@ -18,19 +15,19 @@ def dqt_scaling(factor):
     scaling_dqt = np.around(scaling_dqt)
     return scaling_dqt
 
-def dqt_flat(dqt):
-    flat_dqt = dqt.flatten()
-    return flat_dqt
+def flat(matrix):
+    flat_matrix = matrix.flatten()
+    return flat_matrix
 
-def dqt_zigzag(dqt):
+def zigzag(matrix):
     i=0
     j=0
-    zigzag_dqt = []
-    for x in range(dqt.size):
-        zigzag_dqt.append(dqt[i][j])
-        if (i == 0 or i == dqt.shape[0]-1) and j%2==0:
+    zigzag_matrix= []
+    for x in range(matrix.size):
+        zigzag_matrix.append(matrix[i][j])
+        if (i == 0 or i == matrix.shape[0]-1) and j%2==0:
             j = j + 1
-        elif (j==0 or j==dqt.shape[1]-1) and i%2==1:
+        elif (j==0 or j==matrix.shape[1]-1) and i%2==1:
             i = i + 1
         elif (i+j)%2 == 1 :
             i = i + 1
@@ -39,14 +36,14 @@ def dqt_zigzag(dqt):
             i = i - 1
             j = j + 1
 
-    return zigzag_dqt
+    return zigzag_matrix
 
 if __name__ == '__main__':
     scaling_dqt = dqt_scaling(1)
     print(scaling_dqt)
-    flat_dqt = dqt_flat(scaling_dqt)
+    flat_dqt = flat(scaling_dqt)
     print(10*'*'+'flat dqt'+10*'*')
     print(flat_dqt)
-    zigzag_dqt = dqt_zigzag(scaling_dqt)
+    zigzag_dqt = zigzag(scaling_dqt)
     print(10*'*'+'zigzag dqt'+10*'*')
     print(zigzag_dqt)
